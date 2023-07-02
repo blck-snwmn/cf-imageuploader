22073abfc38be78b2eb20736ef35dda77035c248
blck-snwmn <whitesnowmancreator@gmail.com>
2023-07-02 17:30:34 +0900

    Generate upload page using cf images

--- src/worker.ts
+++ src/worker.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 
 type Bindings = {
 	TOKEN: string
+	ACCOUNT_ID: string
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
@@ -15,23 +16,43 @@ const app = new Hono<{ Bindings: Bindings }>()
 // 	console.log('after')
 // })
 
-app.get('/', (c) => {
-	const url = ""
+
+app.get('/images', async (c) => {
+	const fd = new FormData()
+	fd.append('requireSignedURLs', 'false') // access uploaded image without signed url if false 
+	fd.append('metadata', JSON.stringify({ uploader: 'images' }))
+
+	console.log('account_id', c.env.ACCOUNT_ID)
+	const resp = await fetch(`https://api.cloudflare.com/client/v4/accounts/${c.env.ACCOUNT_ID}/images/v2/direct_upload`, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${c.env.TOKEN}`,
+		},
+		body: fd
+	})
+	if (resp.status !== 200) {
+		const txt = await resp.text()
+		console.log(resp.status, txt)
+		return c.json({ error: txt }, 500)
+	}
+	const jresp = await resp.json() as {
+		result: {
+			id: string
+			uploadURL: string
+		}
+	}
+	const url = jresp.result.uploadURL
 
 	return c.html(`
-<html>
- <body>
-   <form
-     action="${url}"
-     method="post"
-     enctype="multipart/form-data"
-   >
-     <input type="file" id="myFile" name="file" />
-     <input type="submit" />
-   </form>
- </body>
-</html>
-`)
+		<html>
+			<body>
+				<form action="${url}" method="post" enctype="multipart/form-data">
+					<input type="file" id="myFile" name="file" />
+					<input type="submit" />
+				</form>
+			</body>
+		</html>
+	`)
 })
 
 export default app
